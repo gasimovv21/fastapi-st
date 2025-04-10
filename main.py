327cf605cc8a5678e90b44a3c72a999f705ab603
Eltun Gasimov <gasimoweltun@gmail.com>
2025-04-10 20:26:12 +0200

    added validation for name

--- main.py
+++ main.py
@@ -60,7 +60,11 @@ items = [
 
 
 class Item(BaseModel):
-    name: str
+    name: str = Field(
+        min_length=1,  # Minimum length of 1 character
+        max_length=100,  # Maximum length of 100 characters
+        description="Name of the item must be between 1 and 100 characters"
+    )
     price: float = Field(
         ge=0,  # Greater than or equal to 0
         le=10000,  # Less than or equal to 10000
